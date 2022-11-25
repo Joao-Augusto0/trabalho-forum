5ead28a675f71761d7fbf25f847f6d45ec267ec6
batista29 <natabatista2908@gmail.com>
2022-11-25 08:27:19 -0300

    MERxDER atualizado

--- docs/bcd/script.sql
+++ docs/bcd/script.sql
@@ -14,21 +14,13 @@ create table usuarios(
 );
 
 create table Categorias(
-    nome_categoria varchar (20) not null,
-
+    categoria varchar (20) not null,
+    favorito boolean
 );
 
 create table subCategorias(
-    nome varchar (20) not null,
-    informacoes varchar(40)
-);
-
-create table paginas(
-    nome_pagina varchar(30) not null,
-    nome_user varchar(30) not null,
-    data datetime not null,
-    foto_pagina mediumblob,
-    foto_fundo mediumblob
+    subCategoria varchar (20) not null,
+    favorito boolean
 );
 
 create table publicacao(
@@ -40,9 +32,4 @@ create table publicacao(
     coment varchar(200) not null,
     data datetime not null,
     curtidas boolean
-);
-
-create table favorito(
-    nome_pagina varchar(30),
-    nome_categorias varchar(30)
 );
