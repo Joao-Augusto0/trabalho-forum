ffe3437261a8425d3bcd83e5253896facf656161
batista29 <natabatista2908@gmail.com>
2022-12-07 11:02:50 -0300

    Mobile pages

--- mobile/my-app/App.js
+++ mobile/my-app/App.js
@@ -1,78 +1,17 @@
-import { useEffect, useState } from 'react'
-import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native'
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './src/pages/home/index.js';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [posts, setPosts] = useState([])
-
-  useEffect(() => {
-    fetch("http://localhost:3000/Publicacao")
-      .then(res => { return res.json() })
-      .then(data => {
-        setPosts(data)
-      })
-  })
-
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerStyle}>- ASKTALK -</Text>
-      </View>
-      {
-        posts.map((post, index) => {
-          var date = new Date(post.data)
-          var dataFormatadata = date.toLocaleDateString('pt-BR', { timeZone: 'UTC' });
-          const base64Image = post.foto_publi;
-          return (
-            <View key={index} style={styles.publi}>
-              <Text style={styles.texto}>{post.titulo_post}</Text>
-              <Text style={styles.texto}>{post.coment}</Text>
-              <Image style={styles.image} source={{uri: `data:image/jpg;base64,${base64Image}`}} />
-              <Text style={styles.texto}>{post.categoria}</Text>
-              <Text style={styles.texto}>{post.subCategoria}</Text>
-              <Text style={styles.texto}>{dataFormatadata}</Text>
-            </View>
-          )
-        })
-      }
-    </View>
-  )
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
-
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-  },
-  texto: {
-    marginBottom: 5,
-    fontSize: 22,
-    color: 'white',
-    fontFamily: 'Arial',
-  },
-  publi: {
-    height: '230px',
-    width: '370px',
-    border: '1px solid white',
-    marginTop: '30px',
-    backgroundColor: '#a9a9a9a9',
-    textAlign: 'center',
-    justifyContent: 'center'
-  },
-  header: {
-    height: '7vh',
-    width: '100%',
-    backgroundColor: '#a9a9a9',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  headerStyle: {
-    fontSize: '35px',
-    fontFamily: 'Arial',
-  },
-  image:{
-    height:'20px',
-    width:'20px'
-  }
-})
